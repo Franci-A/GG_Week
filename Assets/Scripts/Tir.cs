337f3534afa2b6912151794972c2da3c17ef9fc4
Leo <Aqu4men@gmail.com>
2021-04-12 14:55:32 +0200

    Début du tir et du bullet

--- Assets/Scripts/Tir.cs
+++ Assets/Scripts/Tir.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Tir : MonoBehaviour
 {
+    public GameObject bullet;
+    public Transform muzzle;
+    [SerializeField] float reloadTime;
+    private float cooldown;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cooldown = reloadTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(cooldown <= 0 && Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, muzzle.position, Quaternion.identity);
+        }
+        else
+        {
+            cooldown -= Time.deltaTime;
+        }
     }
 }
