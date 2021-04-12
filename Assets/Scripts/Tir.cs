b61e1c7b779a7c28d2aeb4c734ccb21a7e5a8440
Leo <Aqu4men@gmail.com>
2021-04-12 15:29:40 +0200

    Tir et prefab du bullet

--- Assets/Scripts/Tir.cs
+++ Assets/Scripts/Tir.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Tir : MonoBehaviour
 {
     public GameObject bullet;
+    private GameObject bulletClone;
     public Transform muzzle;
     [SerializeField] float reloadTime;
+    [SerializeField] float bulletSpeed;
     private float cooldown;
+    private SpriteRenderer sR;
     // Start is called before the first frame update
     void Start()
     {
         cooldown = reloadTime;
+        sR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,7 +23,18 @@ public class Tir : MonoBehaviour
     {
         if(cooldown <= 0 && Input.GetButtonDown("Fire1"))
         {
-            Instantiate(bullet, muzzle.position, Quaternion.identity);
+            bulletClone = Instantiate(bullet, muzzle.position, Quaternion.identity);
+            if (sR.flipX)
+            {
+                bulletClone.GetComponent<SpriteRenderer>().flipX = true;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = new Vector2(-bulletSpeed, 0);
+            }
+            else
+            {
+                bulletClone.GetComponent<SpriteRenderer>().flipX = false;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+            }
+            Destroy(bulletClone, 1.0f);
         }
         else
         {
